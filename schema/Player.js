231ee105f0d9f09f82a326239715bc53d0d6dcc3
John <john@akfn.net>
2024-01-04 20:53:22 -0500

    fixed: fixed nickname

--- schema/Player.js
+++ schema/Player.js
@@ -4,7 +4,7 @@ const { Schema } = require('mongoose');
 const playerSchema = new Schema(
   {
     email: { type: String, unique: true, required: true },
-    nickname: { type: String, unique: true, default: `사용자_${randomBytes(4).toString('base64url')}` },
+    nickname: { type: String, unique: true, default: () => `사용자_${randomBytes(4).toString('base64url')}` },
   },
   {
     timestamps: { createdAt: 'created', updatedAt: 'updated' },
