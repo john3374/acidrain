f33136d689eeafdab6a0eb450a2dfc123fcfa890
John Lee <john3374@gmail.com>
2025-11-16 04:46:16 -0500

    fixed: ws host

--- components/socket.js
+++ components/socket.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:4000');
+export const socket = io('https://acidrain.akfn.net');
 export const clientId = Math.random().toString(36).slice(2);
